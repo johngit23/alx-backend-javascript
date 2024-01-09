ce00f9d205fdb9c9c44e14e0c9ee9c674a61077d
johngit23 <126653377+johngit23@users.noreply.github.com>
2024-01-09 19:17:37 +0300

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,5 @@
 /**
  * Retrieves ids from a list of students.
- * @param {{
- *   id: Number,
- *   firstName: String,
- *   location: String
- * }[]} students - The list of students.
- * @returns 
 */
 export default function getListStudentIds(arr) {
   let newArr = [];
