afcf35fd452431dfdf0e3b1041c244e0f1566418
johngit23 <126653377+johngit23@users.noreply.github.com>
2024-02-26 19:27:35 +0300

    Update AppController.js

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,9 +1,10 @@
 /**
  * Contains the miscellaneous route handlers.
+ * @author Yohannes Mebrahtom <https://github.com/johngit23>
  */
 class AppController {
   static getHomepage(request, response) {
-    response.status(200).send("Hello Holberton School!");
+    response.status(200).send('Hello Holberton School!');
   }
 }
 
