fdf9be66018a3e6e604f4475bc2d7b32fefa898a
johngit23 <126653377+johngit23@users.noreply.github.com>
2024-02-26 19:29:27 +0300

    Update StudentsController.js

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,20 +1,21 @@
-import readDatabase from "../utils";
+import readDatabase from '../utils';
 
 /**
  * The list of supported majors.
  */
-const VALID_MAJORS = ["CS", "SWE"];
+const VALID_MAJORS = ['CS', 'SWE'];
 
 /**
  * Contains the student-related route handlers.
+ * @author Yohannes Mebrahtom <https://github.com/johngit23>
  */
 class StudentsController {
   static getAllStudents(request, response) {
-    const dataPath = process.argv.length > 2 ? process.argv[2] : "";
+    const dataPath = process.argv.length > 2 ? process.argv[2] : '';
 
     readDatabase(dataPath)
       .then((studentGroups) => {
-        const responseParts = ["This is the list of our students"];
+        const responseParts = ['This is the list of our students'];
         // A comparison function for ordering a list of strings in ascending
         // order by alphabetic order and case insensitive
         const cmpFxn = (a, b) => {
@@ -27,18 +28,14 @@ class StudentsController {
           return 0;
         };
 
-        for (const [field, group] of Object.entries(studentGroups).sort(
-          cmpFxn
-        )) {
-          responseParts.push(
-            [
-              `Number of students in ${field}: ${group.length}.`,
-              "List:",
-              group.map((student) => student.firstname).join(", "),
-            ].join(" ")
-          );
+        for (const [field, group] of Object.entries(studentGroups).sort(cmpFxn)) {
+          responseParts.push([
+            `Number of students in ${field}: ${group.length}.`,
+            'List:',
+            group.map((student) => student.firstname).join(', '),
+          ].join(' '));
         }
-        response.status(200).send(responseParts.join("\n"));
+        response.status(200).send(responseParts.join('\n'));
       })
       .catch((err) => {
         response
@@ -48,22 +45,20 @@ class StudentsController {
   }
 
   static getAllStudentsByMajor(request, response) {
-    const dataPath = process.argv.length > 2 ? process.argv[2] : "";
+    const dataPath = process.argv.length > 2 ? process.argv[2] : '';
     const { major } = request.params;
 
     if (!VALID_MAJORS.includes(major)) {
-      response.status(500).send("Major parameter must be CS or SWE");
+      response.status(500).send('Major parameter must be CS or SWE');
       return;
     }
     readDatabase(dataPath)
       .then((studentGroups) => {
-        let responseText = "";
+        let responseText = '';
 
         if (Object.keys(studentGroups).includes(major)) {
           const group = studentGroups[major];
-          responseText = `List: ${group
-            .map((student) => student.firstname)
-            .join(", ")}`;
+          responseText = `List: ${group.map((student) => student.firstname).join(', ')}`;
         }
         response.status(200).send(responseText);
       })
